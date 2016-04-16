2a5200b56e441c47dee2a08a9c73824880331c00
Col Harris <col.w.harris@gmail.com>
2016-04-17 02:17:22 +0800

    :rage2:

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule BotHub.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [
-      applications: [:logger, :cowboy, :plug, :gproc, :edeliver, :asn1],
+      applications: [:crypto, :public_key, :ssl, :logger, :cowboy, :plug, :gproc, :edeliver, :asn1],
       mod: {BotHub, []}
     ]
   end
