28c959a85cd90b9447fcb14659bbc0667268d8a4
Col Harris <col.w.harris@gmail.com>
2016-04-20 02:01:35 +0800

    Avoid creating a release (or upgrade) unless this commit being built has been tagged with the correct version.

--- snap_ci_release.sh
+++ snap_ci_release.sh
@@ -4,5 +4,15 @@ export MY_DEPS_PATH=/var/go/deps
 source ~/.kerl/installs/18.2.1/activate
 export PATH=`pwd`/vendor/elixir/bin:$PATH
 
-echo "Build release..."
-mix edeliver build release --skip-existing
+mix do deps.compile, compile
+VERSION=$( mix version )
+echo "Version:" $VERSION
+
+COMMIT_TAG=$(git describe --exact-match HEAD)
+if [ $? == 0 ] && [$COMMIT_TAG == $VERSION]
+then
+  echo "Build release:" $VERSION
+  mix edeliver build release
+else
+  echo "Not a release build"
+fi
--- snap_ci_upgrade.sh
+++ snap_ci_upgrade.sh
@@ -5,17 +5,18 @@ source ~/.kerl/installs/18.2.1/activate
 export PATH=`pwd`/vendor/elixir/bin:$PATH
 
 mix do deps.compile, compile
-NEW_VERSION=$( mix version )
-echo "New Version:" $NEW_VERSION
+VERSION=$( mix version )
+echo "Version:" $VERSION
 
-DEPLOYED_VERSION=$( curl -s http://bothub.colharris.com/version )
-echo "Deployed Version:" $DEPLOYED_VERSION
-
-if [ NEW_VERSION != DEPLOYED_VERSION ]
+COMMIT_TAG=$(git describe --exact-match HEAD)
+if [ $? == 0 ] && [$COMMIT_TAG == $VERSION]
 then
-  mix edeliver build upgrade --with=$DEPLOYED_VERSION --to=$NEW_VERSION
+  DEPLOYED_VERSION=$( curl -s http://bothub.colharris.com/version )
+  echo "Deployed Version:" $DEPLOYED_VERSION
+
+  echo "Build upgrade from $DEPLOYED_VERSION to $VERSION"
+  mix edeliver build upgrade --with=$DEPLOYED_VERSION --to=$VERSION
   mix edeliver deploy upgrade to production
 else
-  mix edeliver build upgrade --with=$DEPLOYED_VERSION
-  mix edeliver deploy upgrade to production
+  echo "Not a release build"
 fi
