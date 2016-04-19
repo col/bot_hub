5ff09331389c3dc3ff212a5ebe46bc139cbab5b5
Col Harris <col.w.harris@gmail.com>
2016-04-19 21:46:26 +0800

    Attempt to auto deploy 0.0.18 to 0.0.19 upgrade to prod server

--- snap_ci_release.sh
+++ snap_ci_release.sh
@@ -1,6 +1,20 @@
+#!/bin/bash
+
 export MY_DEPS_PATH=/var/go/deps
 source ~/.kerl/installs/18.2.1/activate
 export PATH=`pwd`/vendor/elixir/bin:$PATH
 
 echo "Build release..."
 mix edeliver build release
+
+NEW_VERSION=$( mix version )
+echo "New Version:" $NEW_VERSION
+
+DEPLOYED_VERSION=$( curl http://bothub.colharris.com/version )
+echo "Deployed Version:" $DEPLOYED_VERSION
+
+if [ NEW_VERSION != DEPLOYED_VERSION ]
+then
+  mix edeliver build upgrade --from=$DEPLOYED_VERSION --to=$NEW_VERSION
+  mix edeliver deploy upgrade to production
+fi
